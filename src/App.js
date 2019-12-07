5de119f6fa8eb4852c8ab8938a27614fc0a1adc9
casey is on it <caseyeickhoffdesign@gmail.com>
2019-12-07 13:19:55 -0700

    got the day card working with the dummy data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
-import {Container, Row, Col} from 'reactstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import moment from 'moment';
 import SearchBar from './components/SearchBar';
 import DayCard from './components/DayCard';
 import DayDetails from './components/DayDetails';
+import sampleData from './data/sample.json';
 
 
-function App() {
+const App = () => {
+  //using React Hooks for state
+  const [data, setData] = useState({
+    day: sampleData,
+    location: "Denver, CO",
+    searchTerm: ""
+  });
+
+  //destructured for easier calling
+  const { day, location, searchTerm } = data;
+
+
   return (
     <Container>
       <Row>
-        <Col md={8}><h1>Today's Weather</h1></Col>
-        <Col md={4}><SearchBar /></Col>
+        <Col md={8}><h1>Today's Weather for {location}</h1></Col>
+        <Col md={4}><SearchBar
+        /></Col>
       </Row>
       <Row>
-        <Col>
-        <DayCard></DayCard>
+        <Col md={4}>
+          <DayCard
+            day={moment().format('dddd')}
+            current={day.main.temp}
+            high={day.main.temp_max}
+            low={day.main.temp_min}
+            icon={day.weather[0].icon}
+            description={day.weather[0].main}
+          />
+        </Col>
+        <Col md={8}>
+          <DayDetails />
         </Col>
       </Row>
     </Container>
--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { Col, Card, CardHeader, CardBody } from 'reactstrap';
+import styled from 'styled-components';
 
+const DayWrapper = styled.article`
+    .card{
+        border: 2px solid teal;
+        text-align: center;
+    }
+    img {
+        width: auto;
+    }
+`
 
 const DayCard = props => {
 
     return (
-        <span>DayCard</span>
+        <Col>
+            <DayWrapper>
+                <Card>
+                    <CardHeader className="day">{props.day}</CardHeader>
+                    <CardBody>
+                        <h2 className="current">{props.current}&deg;</h2>
+                        <img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt={props.description} />
+                        <h3 className="description">{props.description}</h3>
+                        <p className="hiLow"><strong>High: </strong>{props.high}&deg;  |  <strong>Low: </strong>{props.low}&deg;</p>
+                    </CardBody>
+                </Card>
+            </DayWrapper>
+        </Col>
     )
 }
 
