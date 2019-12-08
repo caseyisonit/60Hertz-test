1fb3d4e1edd6d90c593bc680536b50d63a5b0388
casey is on it <caseyeickhoffdesign@gmail.com>
2019-12-08 08:36:56 -0700

    tieing in the API call

--- src/App.js
+++ src/App.js
@@ -1,64 +1,81 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Jumbotron} from 'reactstrap';
+import { Container, Row, Col, Jumbotron } from 'reactstrap';
 import moment from 'moment';
 import DayCard from './components/DayCard';
 import Clothing from './components/Clothing';
 import NetworkDetector from './hoc/NetworkDetector';
-
-import sampleData from './data/sample.json';
+import API from './utils/API';
 import './App.css'
 
-
 const App = () => {
+
   //using React Hooks for state
-  const [data, setData] = useState({
-    day: sampleData,
-    location: "Denver, CO",
-    searchTerm: "",
+  const [weather, setWeather] = useState({
+    today: {},
+    location: "",
   });
 
   //destructured for easier calling
-  const { day, location, searchTerm } = data;
+  const { today, location } = weather;
 
-  return (
-    <>
-      <Jumbotron>
-        <Row>
-          <img class="logo" src="../images/salom.png" alt="Salom Mulabi Clothing" />
-        </Row>
-        <Row>
-        <h1 className="title">FASHION FORECAST</h1>
-        </Row>
-        <Row>
-        <h3 className="city">{location}</h3>
-        </Row>
-      </Jumbotron>
-      <Container >
-        <Row>
-          <Col md={{size: 8, offset: 2}}>
-          <p className="intro">Welcome to your very own fashion forecast! Don't let weather ruin your holidays this year. We've recommended options for staying stylish and modest that will hold up in any weather condition.</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={4}>
-            <DayCard
-              day={moment().format('dddd')}
-              current={day.main.temp}
-              high={day.main.temp_max}
-              low={day.main.temp_min}
-              icon={day.weather[0].icon}
-              description={day.weather[0].main}
-            />
-          </Col>
-          <Col md={8}>
-            <Clothing
-              description={day.weather[0].main}
-            />
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
-}
+  useEffect(() => {
+    getWeather("80202");
+  }, []);
 
-export default NetworkDetector(App);
+  const getWeather = zip => {
+    
+      API.getWeather(zip)
+        .then(res => {
+          console.log(res);
+          setWeather({
+            today: res.data,
+            location: `${res.data.name}`
+          });
+        })
+        .catch(err => console.log(err));
+        console.log(today)
+        console.log(location)
+  };
+
+    return (
+      <>
+        <Jumbotron>
+          <Row>
+            <img class="logo" src="../images/salom.png" alt="Salom Mulabi Clothing" />
+          </Row>
+          <Row>
+            <h1 className="title">FASHION FORECAST</h1>
+          </Row>
+          <Row>
+            <h3 className="city">{location}</h3>
+          </Row>
+        </Jumbotron>
+        <Container >
+          <Row>
+            <Col md={{ size: 8, offset: 2 }}>
+              <p className="intro">Welcome to your very own fashion forecast! Don't let weather ruin your holidays this year. We've recommended options for staying stylish and modest that will hold up in any weather condition.</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={4}>
+              <DayCard
+                day={moment().format('dddd')}
+              // current={day.main.temp}
+              // high={day.main.temp_max}
+              // low={day.main.temp_min}
+              // icon={day.weather[0].icon}
+              // description={day.weather[0].main}
+              />
+            </Col>
+            <Col md={8}>
+              <Clothing
+              // description={day.weather[0].main}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
+
+  export default NetworkDetector(App);
---
+++ src/utils/API.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const openWeatherKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+
+export default {
+    getWeather: function(zip) {
+        //returning a promised object
+        return axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&APPID=${openWeatherKey}`)
+    }
+}
