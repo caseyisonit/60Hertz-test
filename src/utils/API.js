7e6129d885cf321ce4bd3d5976452e517e85630b
casey is on it <caseyeickhoffdesign@gmail.com>
2019-12-08 11:59:51 -0700

    making the api call and https

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,6 +5,6 @@ const openWeatherKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
 export default {
     getWeather: function(zip) {
         //returning a promised object
-        return axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&APPID=${openWeatherKey}`)
+        return axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&APPID=${openWeatherKey}`)
     }
 }
